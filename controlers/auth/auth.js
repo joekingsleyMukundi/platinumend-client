f65dced7c4bc46a67b67d77b28980bf91cc411b5
joekingsley <joekingsleymukundi@gmail.com>
2022-07-26 14:37:23 +0300

    Chore: Add authentication and cookies

--- controlers/auth/auth.js
+++ controlers/auth/auth.js
@@ -1,6 +1,26 @@
+const axios = require('axios').default;
 exports.getLogin = (req,res,next)=>{
+    if (req.method == 'POST'){
+        console.log(req.body);
+        const {email,password} = req.body
+        const data = {
+            email: email,
+            password: password
+        }
+        axios.post('http://127.0.0.1:8000/api/v1/auth/token/', data)
+        .then(res => {
+            console.log(res.data);
+            
+          return
+        })
+        .catch(err => {
+          console.log(err);
+          return
+        })
+    }
     res.render('login',)
 }
+
 exports.getRegister = (req,res,next)=>{
     res.render('register',)
 }
@@ -8,14 +28,11 @@ exports.getForgotPassword = (req,res,next)=>{
     res.render('forgot-password',)
 }
 exports.getSetForgotPassword = (req,res,next)=>{
-    res.render('set-forgot-password',)
+    res.render('reset-password',)
 }
 exports.getActivateAccount = (req,res,next)=>{
-    res.render('activate-account',)
+    res.render('activate',)
 }
 exports.getBecomeEmployer = (req,res,next)=>{
     res.render('become-employer',)
-}
-exports.getRegisterCompany = (req,res,next)=>{
-    res.render('register-company',)
 }
